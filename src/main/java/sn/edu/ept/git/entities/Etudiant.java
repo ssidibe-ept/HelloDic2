5919ba07e231826a6492dc97b694341a74fba158
ngone881 <ndiayengone@ept.sn>
2025-01-04 09:37:02 +0000

    Création de l'entité Etudiant

--- src/main/java/sn/edu/ept/git/entities/Etudiant.java
+++ src/main/java/sn/edu/ept/git/entities/Etudiant.java
@@ -1,4 +1,20 @@
 package sn.edu.ept.git.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Getter
+@Entity
 public class Etudiant {
+    @Id
+    private Integer id;
+
+    private String nom;
+
+    private String prenom;
+
+    private String telephone;
+
+    private String adresse;
 }
